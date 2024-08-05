7a7fb84b00219786dd1dff093658044185811550
Svitlana Kostohryz <142225792+SvKost@users.noreply.github.com>
2024-08-05 10:48:26 +0300

    language switcher added

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,18 +1,23 @@
 import { NavLink } from 'react-router-dom';
 import css from './WelcomeSection.module.css';
 
+import LangSwitch from '../../components/LangSwitch/LangSwitch';
+import { useTranslation } from 'react-i18next';
 
 export const WelcomeSection = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={css.welcomeSection}>
-      <p className={css.text}>Record daily water intake and track</p>
-      <h1 className={css.title}>Water consumption tracker</h1>
+      <LangSwitch />
+      <p className={css.text}>{t('subtitle')}</p>
+      <h1 className={css.title}>{t('title')}</h1>
       <div className={css.linkBox}>
         <NavLink to="/signup" className={css.linkSignUP}>
-          Try tracker
+          {t('signup')}
         </NavLink>
         <NavLink to="/signin" className={css.linkSignIn}>
-          Sign In
+          {t('signin')}
         </NavLink>
       </div>
     </div>
---
+++ src/i18n/index.js
@@ -0,0 +1,34 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import i18next from 'i18next';
+import { en } from './languages/en';
+import { LOCALES } from './constants';
+import { uk } from './languages/uk';
+
+const resources = {
+  [LOCALES.EN.code]: {
+    translation: en,
+  },
+  [LOCALES.UA.code]: {
+    translation: uk,
+  },
+};
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources,
+    fallbackLng: [LOCALES.EN.code],
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
+
+export const chLang = lang => {
+  i18next.changeLanguage(lang);
+};
